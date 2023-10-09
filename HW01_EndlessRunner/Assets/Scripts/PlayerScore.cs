cd887bbf146f485acb1577b279657a63c42cb2b2
GabrielBall05 <gabrielb4556@gmail.com>
2023-10-09 15:23:47 -0400

    Increase score by 4 per second
    Just increases the player's score by 4 every second

--- HW01_EndlessRunner/Assets/Scripts/PlayerScore.cs
+++ HW01_EndlessRunner/Assets/Scripts/PlayerScore.cs
@@ -6,13 +6,27 @@ using UnityEngine;
 
 public class PlayerScore : MonoBehaviour
 {
+    private float timer;
+    private float delayAmount;
     private static int totalScore;
     public TMP_Text scoreGUI;
 
     // Start is called before the first frame update
     void Start()
     {
+        delayAmount = 1f;
         totalScore = 0;
+    }
+
+    void Update()
+    {
+        //I want to increase the score by 4 every second
+        timer += Time.deltaTime;
+        if (timer >= delayAmount)
+        {
+            timer = 0f;
+            setPlayerScore(1);
+        }
     }
 
     public int getScore()
